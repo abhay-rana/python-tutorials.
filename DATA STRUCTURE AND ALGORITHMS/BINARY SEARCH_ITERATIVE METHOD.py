d58e9fdfcfc96e4e040227733c69aa0371ea0401
Abhay rana <abhayrana286@gmail.com>
2019-03-12 09:42:13 -0700

    ds and algo

---
+++ DATA STRUCTURE AND ALGORITHMS/BINARY SEARCH_ITERATIVE METHOD.py
@@ -0,0 +1,28 @@
+# BINARY SEARCH USING ITERATIVE APPROACH
+# BINARY SEARCH HAS ALSO HAVE A A MODULE IN PYTHON,
+# WHICH CALLED BISECT METHOD
+# remeber the list in sorted order
+
+pos=0
+def binary_search(arr,x):
+    low = 0
+    upper = len(arr) - 1
+    while low<=upper:
+        mid = (low + upper) // 2
+        if arr[mid] == x:
+            globals()['pos']=mid+1   #definig the position of the elemnet globally
+            return True  #if we got the element so stop and return the value
+        elif arr[mid]<x:
+            low=mid+1
+        else:
+            upper=mid-1
+
+    return False
+
+arr=[2,4,6,8,10]       #the sorted list
+x=8          #the element which we have to search in the list
+result=binary_search(arr,x)        #calling the binary search function
+if result==True:
+    print(f"yes,{pos}")
+elif result==False:
+    print("no")
